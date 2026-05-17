942bfbe337fba84405eab8aabaa349fd41315ebf
isistravalon <isistravalonc@gmail.com>
2026-05-17 14:35:00 -0300

    entrega2

--- rdocs/entrega1.R
+++ rdocs/entrega1.R
@@ -22,3 +22,42 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+install.packages("rlang")
+Session -> Restart R
+library(tidyverse)
+install.packages("readxl")
+library(readxl) 
+install.packages("dplyr")
+library(dplyr)
+install.packages("ggplot2")
+library(ggplot2)
+read.csv("pixel110011.xlsx")
+
+infos_jogadores <- read_excel("pixel110011.xlsx", sheet = "infos_jogadores") # dentro desse usar a idade 
+infos_jogadores_jogos <- read_excel("pixel110011.xlsx", sheet = "infos_jogadores_jogos") # dentro desse usar onde_jogam
+
+idade_ondejogam <-(infos_jogadores$idade)$(infos_jogadores_jogo$onde_jogam)
+
+# Fazer o gráfico -> ggplot
+ggplot(idade_ondejogam) +
+  aes(x=factor("idade"), y= onde_jogam) +
+  geom_boxplot(fill=c("#A11D21"), width = 0.5) +
+  guides(fill=FALSE) +
+  stat_summary(fun="mean", geom="point", shape=23, size=3, fill="white") +
+  labs(x="Idade dos jogadores (Anos)", y="Onde os jogadores estam jogando") +
+  theme_estat()
+
+
+ggplot(idade_ondejogam) +
+  aes(x = reorder(onde_jogam, idade, FUN = median), y = idade) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
+  labs(x = "Aparelho eletrônico usado pelos jogadores", y = "Idade dos jogadores (Anos)") +
+  theme_estat()
+ggsave("box_bi.pdf", width = 158, height = 93, units = "mm")
+
+
+
+
+
+         
--- rdocs/entrega2.R
+++ rdocs/entrega2.R
@@ -1,24 +1,48 @@
+
 source("rdocs/source/packages.R")
+readxl::read_excel(
+  +     "C:\\Users\\Alex Sovat\\Documents\\PS - ESTAT\\Projeto-Fantasma-2026.1\\pixel110011.xlsx" )
+library(tidyverse)
+library(lubridate)
+library(dplyr)
 
-# ---------------------------------------------------------------------------- #
+# 2. A receita do jogo “Minecraft” ao longo dos meses de 2025 
+# A cliente gostaria de entender como está sendo a receita total, em dólares, do jogo 
+# “Minecraft” ao longo dos meses de 2025, de janeiro a dezembro, a fim de 
+# compreender se houve aumento ou diminuição na receita produzida ao longo do 
+# tempo. Considere a cotação do dólar a 5,19 reais. 
+# Prazo: 17/05 
 
-#        ______   _____  ________      ________ 
-#      |  ____| / ____| |__   __| /\  |__   __|
-#     | |__    | (___     | |   /  \    | |   
-#    |  __|    \___ \    | |  / /\ \   | |   
-#   | |____   ____) |   | |  /____ \  | |   
-#  |______   |_____/   |_| /_/    \_\|_|   
-#  
-#         Consultoria estatística 
-#
+getwd()
 
-# ---------------------------------------------------------------------------- #
-# ############################## README ###################################### #
-# Consultor, favor utilizar este arquivo .R para realizar as análises
-# alocadas a você neste projeto pelo gerente responsável, salvo instrução 
-# explícita do gerente para mudança.
-#
-# Escreva seu código da forma mais clara e legível possível, eliminando códigos
-# de teste depreciados, ou ao menos deixando como comentário. Dê preferência
-# as funções dos pacotes contidos no Tidyverse para realizar suas análises.
-# ---------------------------------------------------------------------------- #
+library(tidyverse)
+library(lubridate)
+
+library(readxl)
+compras <- read_excel("~/PS - ESTAT/pixel110011.xlsx", sheet = "info_compras")
+jogos <- read_excel("~/PS - ESTAT/pixel110011.xlsx", sheet = "info_jogos")
+analise_mine <- compras %>%
+  left_join(jogos, by = c("gam5_id" = "ga8e_id")) %>%
+  filter(jogo == "Minecraft", year(as.Date(data_compra)) == 2025) %>%
+  mutate(receita_usd = (quantidade * `preco_unitario_R$`) / 5.19) %>%
+  group_by(mes = month(as.Date(data_compra), label = TRUE, abbr = FALSE)) %>%
+  summarise(total_receita_usd = sum(receita_usd, na.rm = TRUE))
+
+print(analise_minecraft)
+
+library(ggplot2)
+grafico_receita <- ggplot(analise_mine) + 
+  aes(x = mes , y = total_receita_usd, group = 1) +
+  geom_line(size = 1, colour = "#a11d21") + geom_point(colour = "#a11d21",
+                                                       size = 2) +
+  labs(x = "Mêses de 2025" , y = "Receita Total") +
+  theme_estat() 
+
+print(grafico_receita)
+
+analise_minecraft %>% 
+  print_quadro_resumo(var_name = total_receita_usd)
+
+
+
+
